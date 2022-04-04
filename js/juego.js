c40f9c41bf5cb0f512aa984c265965ef21310532
deiivid_91 <dssotil@gmail.com>
2022-04-04 16:14:00 +0200

    juego

--- js/juego.js
+++ js/juego.js
@@ -1,6 +1,47 @@
 window.onload = function() {
+    // CREACIÓN DE VARIABLES LOCALES
+    var canvas, ctx;
 
-    var canvas = document.getElementById("SpaceCanvas");
-    var ctx = canvas.getContext("2d");
+
+    // IMPORTACIÓN DE ELEMENTOS DEL DOM
+    canvas = document.getElementById("SpaceCanvas");
+    ctx = canvas.getContext("2d");
+
+    // DEFINICIÓN DE OBJETOS
+
+
+    // MÁS COSILLAS
+    // var drawPicture  = function() {
+    //     // Gestiono el tamaño de la imagen
+    //     let resizeX = 30;
+    //     let resizeY = 30;
+    //     let posX = xBall - (resizeX / 2);
+    //     let posY = yBall - (resizeY / 2);
+    //     // Cargo y muestro la imagen
+    //     var img = new Image();
+    //     img.src = "fifa_ball.png";
+    //     img.onload = function() {
+    //         context.drawImage(img, posX, posY, resizeX, resizeY);
+    //     }
+    // }
+
+    // var keybordDown = function (e) {
+    //     // Arriba 38; abajo 40 --> https://keycode.info/
+    //     // console.log("Se ha llamado al escuchador con la tecla: " + e.keycode);
+    //     switch (e.which) { // e.which --> nº de tecla que lanza el evento
+    //         case 38:
+    //             // console.log("Tecla de arriba");
+    //             stateRaket = -1;
+    //             break;
+    //         case 40:
+    //             // console.log("Tecla de abajo");
+    //             stateRaket = 1;
+    //             break;
+    //         default:
+    //             // console.log("Default");
+    //             break;
+    //     }
+    // }
+    // LINK PARA SACAR LOS CÓDIGOS DE LAS TECLAS: https://keycode.info/
 
 }
