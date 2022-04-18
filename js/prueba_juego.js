84a9a7f482332720f636de99bafa96b1e039a03c
XeKa1500 <100409404@alumnos.uc3m.es>
2022-04-18 19:16:48 +0200

    audio juego y menu terminados

--- js/prueba_juego.js
+++ js/prueba_juego.js
@@ -427,4 +427,26 @@ window.onload = function(){
         console.log("fin función");
     }
     console.log("fin script");
+
+    // AUDIO //
+
+    document.getElementById("play").addEventListener("click",sonar);
+    document.getElementById("stop").addEventListener("click",callar);			
+
+    
+    function sonar(){
+        var sonido = document.createElement("iframe");
+        sonido.setAttribute("src","../res/Chiptronical.ogg");
+        document.body.appendChild(sonido);
+        document.getElementById("play").removeEventListener("click",sonarPajaros);
+    }
+    
+    function callar(){
+        var iframe = document.getElementsByTagName("iframe");
+    
+        if (iframe.length > 0){
+            iframe[0].parentNode.removeChild(iframe[0]);
+            document.getElementById("play").addEventListener("click",sonarPajaros);
+        }
+    }
 }
