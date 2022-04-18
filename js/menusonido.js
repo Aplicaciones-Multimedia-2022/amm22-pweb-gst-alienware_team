8bf2e313948d1c97778428d9ccdecdfacbd0b84e
XeKa1500 <100409404@alumnos.uc3m.es>
2022-04-18 19:04:43 +0200

    musica menu

--- js/menusonido.js
+++ js/menusonido.js
@@ -1,9 +1,23 @@
 window.onload = function (){
 
-    const audioContext = new AudioContext();
-    const element = document.querySelector(audio);
-    const source = audioContext.createMediaElementSource(element);
-    source.connect(audioContext.destination)
-    audio.play();   
+    document.getElementById("play").addEventListener("click",sonar);
+    document.getElementById("stop").addEventListener("click",callar);			
+
+    
+    function sonar(){
+        var sonido = document.createElement("iframe");
+        sonido.setAttribute("src","../res/Chiptronical.ogg");
+        document.body.appendChild(sonido);
+        document.getElementById("play").removeEventListener("click",sonarPajaros);
+    }
+    
+    function callar(){
+        var iframe = document.getElementsByTagName("iframe");
+    
+        if (iframe.length > 0){
+            iframe[0].parentNode.removeChild(iframe[0]);
+            document.getElementById("play").addEventListener("click",sonarPajaros);
+        }
+    }
 
 }
